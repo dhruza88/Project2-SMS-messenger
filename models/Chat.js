aab9cc018f20f4ef7aec757abb48e5d5b414ba16
Jiajie <jiajiehe@yahoo.com>
2022-08-04 23:56:12 -0700

    added user and chat models

---
+++ models/Chat.js
@@ -0,0 +1,31 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Chat extends Model {}
+
+Chat.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'chat',
+  }
+);
+
+module.exports = Chat;
