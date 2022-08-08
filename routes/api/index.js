252415bf49187d9a84ab8c0a515ec9736cc91653
dhruza88 <davidhruza@gmail.com>
2022-08-07 17:57:38 -0700

    merge conflict

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,12 +1,12 @@
 const router = require('express').Router();
-const chatroomRoutes = require('./chatroom-routes');
+const chatroomRoutes = require('./Chat-routes');
 const messageRoutes = require('./message-routes');
 const userRoutes = require('./user-routes');
-const directMsgRoutes = require('./directMsg-routes')
+
 
 router.use('/chatrooms', chatroomRoutes);
 router.use('/messages', messageRoutes);
 router.use('/user', userRoutes);
-router.use('/directMsg', directMsgRoutes);
+
 
 module.exports = router;
