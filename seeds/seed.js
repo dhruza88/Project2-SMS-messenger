3d92088516fbfe8052ae9815f74932bbb082d576
Jiajie <jiajiehe@yahoo.com>
2022-08-08 12:18:07 -0700

    added to seed.js

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,23 +1,24 @@
 const sequelize = require('../config/connection');
-const { User, Chat } = require('../models');
+const { User, Message, Chatroom } = require('../models');
 
 const userData = require('./userData.json');
-const chatData = require('./userData.json');
+const messageData = require('./message.json')
+const chatroomData = require('./chatroomData.json');
 
 const seedDatabase = async () => {
-    await sequelize.sync({ force: true });
+    // await sequelize.sync({ force: true });
   
-    const users = await User.bulkCreate(userData, {
-      individualHooks: true,
-      returning: true,
-    });
+    // const users = await User.bulkCreate(userData, {
+    //   individualHooks: true,
+    //   returning: true,
+    // });
   
-    for (const chat of chatData) {
-      await Chat.create({
-        ...chat,
-        user_id: users[Math.floor(Math.random() * users.length)].id,
-      });
-    }
+    // for (const chatroom of chatroomData) {
+    //   await Chatroom.create({
+    //     ...chatroom,
+    //     user_id: users[Math.floor(Math.random() * users.length)].id,
+    //   });
+    // }
   
     process.exit(0);
   };
