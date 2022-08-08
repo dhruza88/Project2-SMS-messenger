d9c5f9a24842e55c50f00d407f8c88d03eeae712
dhruza88 <davidhruza@gmail.com>
2022-08-07 18:22:08 -0700

    updated index

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,11 +1,9 @@
 const router = require('express').Router();
 const chatroomRoutes = require('./Chat-routes');
-const messageRoutes = require('./message-routes');
 const userRoutes = require('./user-routes');
 
 
 router.use('/chatrooms', chatroomRoutes);
-router.use('/messages', messageRoutes);
 router.use('/user', userRoutes);
 
 
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
+const frontEndRoutes = require('./frontend');
 
+router.use(frontEndRoutes);
 router.use('/api', apiRoutes);
+
 
 router.use((req, res) => {
   res.send("<h1>Wrong Route!</h1>")
