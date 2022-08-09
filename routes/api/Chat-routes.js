2f8ca6b7fb8818e0796d453438c7f234bddffb9a
Santana Fernandez <santanamcf01@gmail.com>
2022-08-08 19:42:00 -0700

    fixing merge conflicts

--- routes/api/Chat-routes.js
+++ routes/api/Chat-routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require("bcrypt")
-const Chat = require('../../models/chat');
+const Chat = require('../../models/chatroom');
 const { User } = require('../../models');
 const app = express();
 const server = require('http').Server(app)
