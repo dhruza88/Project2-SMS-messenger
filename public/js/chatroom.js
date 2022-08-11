84f2b23fc7b53b8ca79f26fa2db2e337452d0d16
dhruza88 <davidhruza@gmail.com>
2022-08-10 21:27:03 -0700

    trying to fix chat route

--- public/js/chatroom.js
+++ public/js/chatroom.js
@@ -77,11 +77,15 @@
 //     });
 //   }
 
-const express = require('express');
+// const path = require('path');
+import express from 'express';
+import session from 'express-session';
+import exphbs from 'express-handlebars';
+import routes from './routes';
 const app = express();
-const http = require('http');
-const server = http.createServer(app);
-const { Server } = require("socket.io");
+import { createServer } from 'http';
+const server = createServer(app);
+import { Server } from "socket.io";
 const io = new Server(server);
 
 app.get('/', (req, res) => {
