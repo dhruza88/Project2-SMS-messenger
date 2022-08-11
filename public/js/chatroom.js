50083a58f4299632c3af140fbc09dc192a7247e8
dhruza88 <davidhruza@gmail.com>
2022-08-11 07:21:21 -0700

    not firing twice with socket.io

--- public/js/chatroom.js
+++ public/js/chatroom.js
@@ -1,6 +1,6 @@
 // https://github.com/bradtraversy/chatcord/blob/master/public/js/main.js socket.io emit/receive
 
-const socket = io("");
+const socket = io();
 
 var messages = document.getElementById('messages');
 var form = document.getElementById('form');
@@ -13,7 +13,7 @@ form.addEventListener('submit', function (e) {
     input.value = '';
   }
 
-  socket.on('chat message', function (msg) {
+  socket.once('chat message', function (msg) {
     var item = document.createElement('li');
     item.textContent = msg;
     messages.appendChild(item);
@@ -21,9 +21,3 @@ form.addEventListener('submit', function (e) {
   });
 });
 
-// socket.on('chat message', function (msg) {
-//     var item = document.createElement('li');
-//     item.textContent = msg;
-//     messages.appendChild(item);
-//     window.scrollTo(0, document.body.scrollHeight);
-// });
