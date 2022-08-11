7179498315ebb8a75eda1e7fd6ff33dc8168fc73
dhruza88 <davidhruza@gmail.com>
2022-08-10 22:59:52 -0700

    commented out helper

--- utils/helpers.js
+++ utils/helpers.js
@@ -13,23 +13,23 @@
 //     },
 //   };
 
- var socket = io();
+//  var socket = io();
 
-var messages = document.getElementById('messages');
-var form = document.getElementById('form');
-var input = document.getElementById('input');
+// var messages = document.getElementById('messages');
+// var form = document.getElementById('form');
+// var input = document.getElementById('input');
 
-form.addEventListener('submit', function (e) {
-    e.preventDefault();
-    if (input.value) {
-        socket.emit('chat message', input.value);
-        input.value = '';
-    }
-});
+// form.addEventListener('submit', function (e) {
+//     e.preventDefault();
+//     if (input.value) {
+//         socket.emit('chat message', input.value);
+//         input.value = '';
+//     }
+// });
 
-socket.on('chat message', function (msg) {
-    var item = document.createElement('li');
-    item.textContent = msg;
-    messages.appendChild(item);
-    window.scrollTo(0, document.body.scrollHeight);
-});
+// socket.on('chat message', function (msg) {
+//     var item = document.createElement('li');
+//     item.textContent = msg;
+//     messages.appendChild(item);
+//     window.scrollTo(0, document.body.scrollHeight);
+// });
