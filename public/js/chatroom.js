b48d26d5f293dc84552137e914ecc1675e1f24e7
Jiajie <jiajiehe@yahoo.com>
2022-08-12 16:45:50 -0700

    message bug fixed

--- public/js/chatroom.js
+++ public/js/chatroom.js
@@ -13,11 +13,11 @@ form.addEventListener('submit', function (e) {
     input.value = '';
   }
 
-  socket.once('chat message', function (msg) {
-    var item = document.createElement('li');
-    item.textContent = msg;
-    messages.appendChild(item);
-    window.scrollTo(0, document.body.scrollHeight);
-  });
+});
+socket.on('chat message', function (msg) {
+  var item = document.createElement('li');
+  item.textContent = msg;
+  messages.appendChild(item);
+  window.scrollTo(0, document.body.scrollHeight);
 });
 
